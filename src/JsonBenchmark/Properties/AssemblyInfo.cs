4920f8fa71c47cac7694ae7af6a62a0f99ddbcb5
Adrian Macal <adma@amacal.pl>
2015-06-18 19:54:26 +0200

    Add solution with a project.

---
+++ src/JsonBenchmark/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("JsonBenchmark")]
+[assembly: AssemblyProduct("JsonBenchmark")]
+[assembly: AssemblyCopyright("Copyright © Adrian Macal 2015")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
